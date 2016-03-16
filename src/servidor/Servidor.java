d0dc7a547deb33006f5ef6f0bf8dfd2961ef2699
Xcode23 <joaofarinha10@hotmail.com>
2016-03-16 19:33:43 +0000

    Multi-Threaded_Server0.2

--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -1,30 +1,21 @@
 package servidor;
 
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 
 
-public class Servidor extends Thread {
-    
+public class Servidor{
+
     public static void main(String[] args) throws Exception {
 
+        HashMap<String,Utilizador> tabela=new HashMap<String,Utilizador>();
         ServerSocket serverSocket = new ServerSocket(6789);
 
         while(true){
             Socket connectionSocket = serverSocket.accept();
-
-            InputStream in = connectionSocket.getInputStream();
-
-            byte[] b = new byte[8];
-
-            in.read(b);
-
-            System.out.println("Recebi: " + b.toString());
-
-            OutputStream out = connectionSocket.getOutputStream();           
-            out.write(b);
+            ServidorThread t=new ServidorThread(tabela,connectionSocket);
+            t.start();
         }
     }
 }
